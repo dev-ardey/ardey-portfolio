29d16dc62a07e992faae7b69be58a94b15deff46
dev-ardey <87880864+dev-ardey@users.noreply.github.com>
2023-07-14 23:46:19 +0200

    Add files via upload
    working button js

--- app.js
+++ app.js
@@ -22,3 +22,31 @@ const hiddenElements = document.querySelectorAll('.hidden');
 
 // tell the observer what to observe, loop over each elements and observe all hidden elements
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+
+// skill - list transform functions 
+
+document.addEventListener('DOMContentLoaded', function () {
+    var skillList = document.getElementById('skill-list');
+    skillList.style.display = 'none';
+});
+
+document.getElementById('checkbox').onclick = function () {
+    toggleSkillList();
+};
+
+function toggleSkillList() {
+    var checkbox = document.getElementById('checkbox');
+    var skillList = document.getElementById('skill-list');
+
+    if (checkbox.checked) {
+        skillList.style.display = 'block';
+    } else {
+        skillList.style.display = 'none';
+    }
+}
+
